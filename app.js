65650251cd2b1a2a8808d8e6ef178ba00ec7910b
ZachYentsch <z6chy@icloud.com>
2021-12-06 16:32:58 -0700

    push

---
+++ app.js
@@ -0,0 +1,14 @@
+function single() {
+    document.getElementById('team1').innerText = team1++
+}
+function single1() {
+    document.getElementById('team2').innerText = team2++
+}
+let team1 = 0
+let team2 = 0
+let phrase = 'hit'
+
+// const team = {
+//     tm1: { name: 'Savannah Bananas', }
+//     tm2: { name: 'Macon Bacons', }
+// }
